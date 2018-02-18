ab2c4b0046aa8adc558dc0a850c6e9bd13219fc6
Lynna Orm <lynnaorm@gmail.com>
2018-02-17 20:41:44 -0800

    Declaring Variables

---
+++ IDoDeclare.js
@@ -0,0 +1,16 @@
+var num1 = 56; 
+var num2 = 24;        /*numbers*/ 
+var num3 = 21;
+var num4 = 21;
+
+var string1 = "Bob";
+var string2 = "likes";           /*strings*/
+var string3 = "chocolate";
+
+var winteriscold = true;        /*booleans*/
+var summeriscold = false;
+
+var x = /*undefined*/
+
+console.log(num1, num2, num3, num4, string1, string2, string3, winteriscold, summeriscold, x);
+
